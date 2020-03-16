13cd8731f523794074161d27737ba2be78068b01
r-przezminski <32273937+r-przezminski@users.noreply.github.com>
2020-03-16 13:13:40 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# gsap-animations
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactComponent as Scene } from "./../public/assets/images/scene.svg";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Scene />
+    </div>
+  );
+}
